155e5b5cacd9bd97364c87a59ca362db53d7eb55
Kiyanna Gayle <Kgayl008@fiu>
2023-05-13 13:20:09 -0400

    linked photo page to the next button on the name component

--- src/pages/Name/Name.js
+++ src/pages/Name/Name.js
@@ -1,26 +1,28 @@
 import './Name.scss';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 import IconButton from "@mui/material/IconButton";
-import { Link } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import Logo from '../../assets/Logo/wanderly.png'
 
 const Name = () => {
-    return(
+    return (
         <div className='name'>
             <header>
-                <img src={Logo} alt='Wanderly logo' className='name__logo'/>
+                <img src={Logo}
+                    alt='Wanderly logo'
+                    className='name__logo'/>
             </header>
             <h1 className='name__question'>What's your first name?</h1>
             <input type="text" className='name__input'></input>
             <div className='next'>
                 <Link to='/photo'>
-                <IconButton>
-                <ArrowForwardIosIcon fontSize='large' />
-                </IconButton>
+                    <IconButton>
+                        <ArrowForwardIosIcon fontSize='large'/>
+                    </IconButton>
                 </Link>
             </div>
         </div>
-        
+
     )
 }
 
